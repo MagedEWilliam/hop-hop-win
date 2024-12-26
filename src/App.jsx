2c997c9aeb5478619c7e3ae8ef77a4fb55415b82
Maged E. William <MagedE.William@gmail.com>
2024-12-27 01:53:19 +0200

    add a grid, in fullscreen, use biom

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,117 @@
 import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
-import { currentMonitor } from '@tauri-apps/api/window';
-import { register } from '@tauri-apps/plugin-global-shortcut';
+import {
+	currentMonitor,
+	getCurrentWindow,
+	LogicalPosition,
+	LogicalSize,
+} from "@tauri-apps/api/window";
+import { register, unregister } from "@tauri-apps/plugin-global-shortcut";
 import "./App.css";
 
-const monitor = currentMonitor();
+function Cell({ index, cell }) {
+	return (
+		<div
+			key={index}
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				border: "1px solid #ccc",
+				fontSize: "12px",
+				boxSizing: "border-box",
+			}}
+		>
+			<p>{cell}</p>
+		</div>
+	);
+}
+
+function Cells() {
+	const gridSize = 26; // 28x28 grid
+	const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	const letterPairs = [];
+	// Generate pairs of letters for each cell
+	for (let i = 0; i < gridSize; i++) {
+		for (let j = 0; j < gridSize; j++) {
+			const pair = alphabet.charAt(i) + alphabet.charAt(j);
+			letterPairs.push(pair);
+		}
+	}
+
+	return (
+		<div className="grid-container">
+			{letterPairs.map((pair, index) => (
+				<div key={index} className="grid-item">
+					<div className="cordinates">
+						<p className="left">{pair[0]}</p>
+						<p className="right">{pair[1]}</p>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+}
 
 function App() {
-  const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
+	const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
 
-  async function move_mouse(x=0, y=0, abs=false) {
-    try {
-      setGreetMsg(await invoke("move_mouse", { x, y, abs}));
-    } catch (e) {
-      console.log(e);
-    }
-  }
+	async function move_mouse(x = 0, y = 0, abs = false) {
+		try {
+			setGreetMsg(await invoke("move_mouse", { x, y, abs }));
+		} catch (e) {
+			console.log(e);
+		}
+	}
 
-  async function hideWindow() {
-    try {
-      await invoke("hide_window");
-    } catch (e) {
-      console.log(e);
-    }
-  }
+	async function hideWindow() {
+		try {
+			await invoke("hide_window");
+		} catch (e) {
+			console.log(e);
+		}
+	}
 
-  useEffect( () => {
-    (async function() {
-      await register('CommandOrControl+Shift+Space', async () => {
-        console.log('Shortcut triggered');
-        await invoke("show_window");
-      });
+	useEffect(() => {
+		async function getWindow() {
+			const monitor = currentMonitor();
+			const theWindow = await monitor;
+			await getCurrentWindow().setSize(
+				new LogicalSize(theWindow.size.width, theWindow.size.height),
+			);
+			await getCurrentWindow().setPosition(
+				new LogicalPosition(theWindow.position.x, theWindow.position.y),
+			);
+			await getCurrentWindow().setFullscreen(true);
+			console.log(await getCurrentWindow(), theWindow.size);
+			setScreenSize(theWindow.size);
+		}
+		getWindow();
 
-      const theWindow = await monitor;
-      console.log(theWindow);
-      setScreenSize(theWindow.size);
-    })();
-  }, []);
+		async function registerShortcuts() {
+			await register("CommandOrControl+Shift+Alt+Tab", async () => {
+				console.log("Shortcut triggered");
+				await invoke("show_window");
+			});
+		}
 
-  return (
-    <div className="container" onClick={hideWindow}>
-      <h1>Welcome to Tauri + React</h1>
-    </div>
-  );
+		function unregisterAll() {
+			unregister("CommandOrControl+Shift+Alt+Tab");
+		}
+
+		// registerShortcuts();
+
+		return () => {
+			unregisterAll();
+		};
+	}, []);
+
+	return (
+		<div className="container">
+			<Cells monitorSize={screenSize} />
+		</div>
+	);
 }
 
 export default App;
